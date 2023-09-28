700b0285364a0745dcab35abaf68c55ae6ad920f
Nir Izhak <nir676676@gmail.com>
2023-09-28 19:36:38 +0300

    Add UI

--- src/screens/ShirtsScreen.jsx
+++ src/screens/ShirtsScreen.jsx
@@ -1,10 +1,49 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
+import React, { useContext } from "react";
+import { SellMeContext } from "../context/SellMeContext";
+import ItemCard from "../components/ItemCard";
 
-export default function ShirtsScreen() {
+export default function ShoesScreen({ navigation }) {
+  const { allShirts } = useContext(SellMeContext);
+
   return (
-    <View>
-      <Text>ShirtsScreen</Text>
-    </View>
-  )
+    <SafeAreaView style={styles.container}>
+      <Text
+        style={{
+          textAlign: "center",
+          marginTop: 15,
+          fontSize: 30,
+          fontWeight: "bold",
+        }}
+      >
+        חולצות
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "500",
+          fontSize: 20,
+        }}
+      >
+        פריטים זמינים = {allShirts.length}
+      </Text>
+      <FlatList
+        data={allShirts}
+        renderItem={({ item }) => (
+          <ItemCard item={item} navigation={navigation} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        style={{ marginTop: 30 }}
+      />
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  itemContainer: {
+    display: "flex",
+  },
+});
