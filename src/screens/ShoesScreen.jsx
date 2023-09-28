b0b6255b24ed09e975014298a56274d1201e1387
Nir Izhak <nir676676@gmail.com>
2023-09-28 19:36:31 +0300

    Add UI

--- src/screens/ShoesScreen.jsx
+++ src/screens/ShoesScreen.jsx
@@ -3,20 +3,30 @@ import React, { useContext } from "react";
 import { SellMeContext } from "../context/SellMeContext";
 import ItemCard from "../components/ItemCard";
 
-export default function ShoesScreen() {
+export default function ShoesScreen({ navigation }) {
   const { allShoes } = useContext(SellMeContext);
-  console.log(allShoes);
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>ShoesScreen</Text>
-      <Text>Items = {allShoes.length}</Text>
+      <Text  style={{
+          textAlign: "center",
+          marginTop: 15,
+          fontSize: 30,
+          fontWeight: "bold",
+        }}>נעליים</Text>
+      <Text style={{
+        textAlign: 'center',
+        fontWeight: '500',
+        fontSize: 20
+      }}>פריטים זמינים = {allShoes.length}</Text>
       <FlatList
-      data={allShoes}
-      renderItem={({ item }) => <ItemCard item={item} />}
-      keyExtractor={(item, index) => index.toString()}
-      style={{ marginTop: 30 }}
-    />
+        data={allShoes}
+        renderItem={({ item }) => (
+          <ItemCard item={item} navigation={navigation} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        style={{ marginTop: 30 }}
+      />
     </SafeAreaView>
   );
 }
@@ -26,6 +36,6 @@ const styles = StyleSheet.create({
     flex: 1,
   },
   itemContainer: {
-display: 'flex'
+    display: "flex",
   },
 });
