9fd6c799f3942ebc9a32c23717335289b4b9dd81
Nir Izhak <nir676676@gmail.com>
2023-09-30 21:28:55 +0300

    add search bar

--- src/screens/ShoesScreen.jsx
+++ src/screens/ShoesScreen.jsx
@@ -1,31 +1,50 @@
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { View, Text, SafeAreaView, StyleSheet, FlatList, TextInput } from "react-native";
 import { SellMeContext } from "../context/SellMeContext";
 import ItemCard from "../components/ItemCard";
 
 export default function ShoesScreen({ navigation }) {
   const { allShoes } = useContext(SellMeContext);
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredShoes = allShoes.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    item.brand.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text  style={{
+      <Text
+        style={{
           textAlign: "center",
           marginTop: 15,
           fontSize: 30,
           fontWeight: "bold",
-        }}>נעליים</Text>
-      <Text style={{
-        textAlign: 'center',
-        fontWeight: '500',
-        fontSize: 20
-      }}>פריטים זמינים = {allShoes.length}</Text>
+        }}
+      >
+        נעליים
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "500",
+          fontSize: 20,
+        }}
+      >
+        פריטים זמינים = {filteredShoes.length}
+      </Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search..."
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+      />
       <FlatList
-        data={allShoes}
+        data={filteredShoes}
         renderItem={({ item }) => (
           <ItemCard item={item} navigation={navigation} />
         )}
         keyExtractor={(item, index) => index.toString()}
-        style={{ marginTop: 30 }}
+        style={{ marginTop: 10 }}
       />
     </SafeAreaView>
   );
@@ -34,6 +53,13 @@ export default function ShoesScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+  },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    paddingLeft: 10,
   },
   itemContainer: {
     display: "flex",
