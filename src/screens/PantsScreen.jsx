31798184ae5ac5f5c87b233d83328d9e8ace6f21
Nir Izhak <nir676676@gmail.com>
2023-09-30 21:28:45 +0300

    add search bar

--- src/screens/PantsScreen.jsx
+++ src/screens/PantsScreen.jsx
@@ -1,30 +1,51 @@
-import { View, Text, SafeAreaView, StyleSheet, FlatList } from "react-native";
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { View, Text, SafeAreaView, StyleSheet, FlatList, TextInput } from "react-native";
 import { SellMeContext } from "../context/SellMeContext";
 import ItemCard from "../components/ItemCard";
 
-export default function ShoesScreen({navigation}) {
+export default function ShoesScreen({ navigation }) {
   const { allPants } = useContext(SellMeContext);
+  const [searchTerm, setSearchTerm] = useState("");
+  const filteredPants = allPants.filter(item =>
+    item.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+    item.brand.toLowerCase().includes(searchTerm.toLowerCase())
+  );
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={{
+      <Text
+        style={{
           textAlign: "center",
           marginTop: 15,
           fontSize: 30,
           fontWeight: "bold",
-        }}>מכנסיים</Text>
-      <Text style={{
-        textAlign: 'center',
-        fontWeight: '500',
-        fontSize: 20
-      }}>פריטים זמינים = {allPants.length}</Text>
+        }}
+      >
+        מכנסיים
+      </Text>
+      <Text
+        style={{
+          textAlign: "center",
+          fontWeight: "500",
+          fontSize: 20,
+        }}
+      >
+        פריטים זמינים = {filteredPants.length}
+      </Text>
+      <TextInput
+        style={styles.searchInput}
+        placeholder="Search..."
+        value={searchTerm}
+        onChangeText={setSearchTerm}
+      />
       <FlatList
-      data={allPants}
-      renderItem={({ item }) => <ItemCard item={item} navigation={navigation} />}
-      keyExtractor={(item, index) => index.toString()}
-      style={{ marginTop: 30 }}
-    />
+        data={filteredPants}
+        renderItem={({ item }) => (
+          <ItemCard item={item} navigation={navigation} />
+        )}
+        keyExtractor={(item, index) => index.toString()}
+        style={{ marginTop: 10 }}
+      />
     </SafeAreaView>
   );
 }
@@ -33,7 +54,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
   },
+  searchInput: {
+    height: 40,
+    borderColor: 'gray',
+    borderWidth: 1,
+    margin: 10,
+    paddingLeft: 10,
+  },
   itemContainer: {
-display: 'flex'
+    display: "flex",
   },
 });
